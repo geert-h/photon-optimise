8f87d2a279ea520833e16a404614dd7d79bb8cd9
vicgeentor <github@vicgeentor.nl>
2026-06-11 12:28:51 +0200

    chore: add nix development shell files

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              cargo
+              clippy
+              pkg-config
+              nodejs
+              rustc
+              rustfmt
+              rust-analyzer
+
+              # Debug/profiling tools
+              hotspot
+              perf
+            ];
+            env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+        };
+    };
+}
